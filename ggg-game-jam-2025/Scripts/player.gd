7a5ef1b7de3d705230a796d9bf21dda112cc5ded
TriumphInDeath <declancwhitlock@gmail.com>
2025-08-16 18:03:13 -0700

    Lots of setup
    Worked on player, began tweaking gravity and jump strength
    Created and started work on Unstable Platforms
    Created Test Scene and Fridge background for scale

--- ggg-game-jam-2025/Scripts/player.gd
+++ ggg-game-jam-2025/Scripts/player.gd
@@ -2,13 +2,14 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -1000.0
+const GRAVITY = 2000 # default gravity is 980
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += GRAVITY * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -23,3 +24,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	
+	
