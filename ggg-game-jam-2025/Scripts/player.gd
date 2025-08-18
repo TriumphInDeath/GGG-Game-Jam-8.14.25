d1e84f389e3f3429a1d82afe8d4239a440cb5572
TriumphInDeath <declancwhitlock@gmail.com>
2025-08-18 01:13:15 -0700

    Temp AnimatedSprite
    Added temp sprite sheet, added animations to spriteFrames and set up playing in Player script

--- ggg-game-jam-2025/Scripts/player.gd
+++ ggg-game-jam-2025/Scripts/player.gd
@@ -5,6 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -1000.0
 const GRAVITY = 2000 # default gravity is 980
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -20,6 +21,8 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animated_sprite.flip_h = velocity.x > 0
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
@@ -27,3 +30,12 @@ func _physics_process(delta: float) -> void:
 	
 	
 	
+	# Play animations
+	if is_on_floor():
+		if not direction == 0: animated_sprite.play("running")
+		if randi_range(0, 99) == 0: animated_sprite.play("blink")
+		elif not animated_sprite.is_playing(): animated_sprite.play("still")
+		
+	# else:
+		# if velocity.y < 0: animated_sprite.play("Jump")
+		# else: animated_sprite.play("Fall")
