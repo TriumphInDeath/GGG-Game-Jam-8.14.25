8f6c8071034b5b536727d8a6680ffe644f08c8cc
TriumphInDeath <declancwhitlock@gmail.com>
2025-08-19 15:34:26 -0700

    Model and Movement Update
    New character sprite sheets added and set up
    Character controls updated to no longer use ui_left/right, along with adding coyote time
    Not yet merging to main, as I plan to continue working after a break

--- ggg-game-jam-2025/Scripts/player.gd
+++ ggg-game-jam-2025/Scripts/player.gd
@@ -5,20 +5,28 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -1000.0
 const GRAVITY = 2000 # default gravity is 980
 
+var _airTime = 0
+var _canJump = true
+
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
+		_airTime += 1
+	else:
+		_airTime = 0
+		_canJump = true
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and _airTime < 9 and _canJump:
 		velocity.y = JUMP_VELOCITY
+		_canJump = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 		animated_sprite.flip_h = velocity.x > 0
@@ -33,8 +41,7 @@ func _physics_process(delta: float) -> void:
 	# Play animations
 	if is_on_floor():
 		if not direction == 0: animated_sprite.play("running")
-		if randi_range(0, 99) == 0: animated_sprite.play("blink")
-		elif not animated_sprite.is_playing(): animated_sprite.play("still")
+		else: animated_sprite.play("idle")
 		
 	# else:
 		# if velocity.y < 0: animated_sprite.play("Jump")
--- ggg-game-jam-2025/Scripts/unstable_platform.gd
+++ ggg-game-jam-2025/Scripts/unstable_platform.gd
@@ -1,5 +1,6 @@
 extends AnimatableBody2D
 
+@onready var collapsing = false
 # @onready var animated_sprite = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
@@ -13,14 +14,14 @@ func _process(delta: float) -> void:
 
 
 func _on_stood_on_body_entered(body: Node2D) -> void:
-	pass # start platform shaking/showing signs of falling
+	print("enter") # start platform shaking/showing signs of falling
 
 
 
 
 func _on_stood_on_body_exited(body: Node2D) -> void:
-	pass # if not already falling, make platform fall
+	print("exit") # if not already falling, make platform fall
 
 
 func _collapse() -> void:
-	pass # transform.y += 100
+	print("collapse") # transform.y += 100
