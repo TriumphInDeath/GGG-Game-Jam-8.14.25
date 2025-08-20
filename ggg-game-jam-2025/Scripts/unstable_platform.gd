47bec9681e0acc7945c3434c16bd95b8adaef4f7
TriumphInDeath <declancwhitlock@gmail.com>
2025-08-19 19:15:40 -0700

    UnstablePlatform progress
    beta working unstablePlatform, stopping for dinner

--- ggg-game-jam-2025/Scripts/unstable_platform.gd
+++ ggg-game-jam-2025/Scripts/unstable_platform.gd
@@ -1,7 +1,12 @@
 extends AnimatableBody2D
 
-@onready var collapsing = false
-# @onready var animated_sprite = $AnimatedSprite2D
+const SHAKE = 5
+
+@onready var _shaking = false
+@onready var _collapsed = false
+
+@onready var collapse_timer = $CollapseTimer
+@onready var sprite = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,18 +15,32 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if (_shaking):
+		
+		if (randi_range(0, 5) == 0):
+			sprite.transform.origin += Vector2(randi_range(-SHAKE, SHAKE), randi_range(-SHAKE, SHAKE))
+	if (_collapsed):
+		transform.origin.y += 10
 
 
 func _on_stood_on_body_entered(body: Node2D) -> void:
-	print("enter") # start platform shaking/showing signs of falling
-
-
+	if (not _shaking and body.name == "Player"):
+		_shake()
+		collapse_timer.start()
 
 
 func _on_stood_on_body_exited(body: Node2D) -> void:
-	print("exit") # if not already falling, make platform fall
+	if (not _collapsed): _collapse()
 
+
+func _on_collapse_timer_timeout() -> void:
+	if (not _collapsed): _collapse()
+
+func _shake() -> void:
+	_shaking = true
+	print("shake")
 
 func _collapse() -> void:
+	_collapsed = true
+	_shaking = false
 	print("collapse") # transform.y += 100
